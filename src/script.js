3ef69e6024340bdc28f5291fb8b3b65b1ed97573
kiranashfaq <ahmedmemon4123@gmail.com>
2024-06-27 00:54:36 +0500

    second commit

--- src/script.js
+++ src/script.js
@@ -1,30 +1,42 @@
 var addBtn = document.getElementById("add-btn")
-var addInput = document.getElementById("add-inp");
-var textInput = document.getElementById("text-inp");
+var addInput = document.getElementById("add-inp"); // input box
+var textInput = document.getElementById("text-inp"); // to do list
 
 
 
 addBtn.addEventListener("click", function() {
-    // input.innerHTML += `<div class="exist-div"><input class="text-inp" disabled ${textInput.value}> <button class="edit">Edit</button> <button class="edit">delete</button></div>`
+    // for empty input 
+var emptyInp = textInput.value
+    if(emptyInp.length <= 0){
+      alert("You must need to write something");
+      return false;
+    }
+    // for adding task 
 var editBtn = `<button class="edit-del" id="edit">Edit</button>`;
 var delBtn = `<button class="edit-del" id="del">delete</button>`;
-addInput.innerHTML += `<div class="main-div" id="div" ><div class="edit-div" id="edit-inp">${textInput.value}</div>${editBtn} ${delBtn}</div>`;
-// input.innerHTML += `<div class="exist-div" id="div" ><input disabled class="new-div" id="edit-inp" value="${textInput.value}">${editBtn} ${delBtn}</div>`;
 
+addInput.innerHTML += `<div class="main-div" id="div" >
+<div class="edit-div" id="edit-inp">
+${textInput.value}</div>
+${editBtn} ${delBtn}</div>`;
 
-        // input.innerHTML += `<div class="exist-div"><div class="new-div">${textInput.value}</div><button class="edit-del">Edit</button> <button class="edit-del">delete</button></div>`;
+// del button 
 
 var del = document.getElementById("del");
 var div = document.getElementById("div");
+// edit btn 
 var edit = document.getElementById("edit");
 var editInp = document.getElementById("edit-inp");
 
+// del functionality 
 del.addEventListener("click", function () {
-   div.innerHTML = "";
+    div.innerHTML = "";
+
 
 });
+// edit functionality 
 edit.addEventListener("click" , function(){
-    editInp.innerHTML = `<input class="new-inp" type="text"  value="${textInput.value}">`;
+    editInp.innerHTML = `<input class="new-inp" type="text"  value="${textInput.value}">`
 })
 
 
